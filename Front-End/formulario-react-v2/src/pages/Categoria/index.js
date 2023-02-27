9216d78399cdbfe1ea43c8910f0e5c2a68f18781
InoueAkiraa <akiraij@hotmail.com>
2023-02-27 16:49:14 -0400

    Mudança de código
    Implementada funcionalidade de criação de um novo registro, ainda é necessário finalizar a funcionalidade de alteração.

--- Front-End/formulario-react-v2/src/pages/Categoria/index.js
+++ Front-End/formulario-react-v2/src/pages/Categoria/index.js
@@ -1,63 +1,63 @@
-import React, {useState, useEffect} from "react";
-import {Link, Navigate} from 'react-router-dom';
-import './index.css';
-import logoCadastro from '../../assets/etiquetas.png';
-import {FiXCircle, FiEdit, FiUserX} from 'react-icons/fi';
-import api from '../../services/api';
+    import React, {useState, useEffect} from "react";
+    import {Link, useNavigate} from 'react-router-dom';
+    import './index.css';
+    import logoCadastro from '../../assets/etiquetas.png';
+    import {FiXCircle, FiEdit, FiUserX} from 'react-icons/fi';
+    import api from '../../services/api';
 
-export function Categorias(){
-    const [categorias, setCategorias] = useState([]);    
+    export function Categorias(){
+        const [categorias, setCategorias] = useState([]);    
+        const navigate = useNavigate();
+        useEffect( () => {
+            api.get('https://localhost:7290/api/supermercado/Categoria').then(
+                response => {setCategorias(response.data);
+                }
+            )
+        })
 
-    useEffect( () => {
-        api.get('https://localhost:7290/api/supermercado/Categoria').then(
-            response => {setCategorias(response.data);
+        async function editCategoria(categoriaId){
+            try {
+                navigate(`/categoria/novo/${categoriaId}`);
+            }catch(error){
+            alert('Erro ao recuperar os dados');
             }
-        )
-    })
-
-    async function editAluno(id){
-        try {
-            Navigate(`categoria/novo/${id}`);
-        }catch(error){
-        alert('Erro ao recuperar os dados');
         }
-    }
 
-    return (
-        <div className="categoria-container">
-            <header>
-                <img src={logoCadastro} alt="Cadastro"/>
-                <Link className="button" to="/categoria/novo/0">Nova Categoria</Link>
-                <button type="button">
-                    <FiXCircle size={40} color="#17202a"/>
-                </button>
-            </header>
-
-            <form className="categoria-form">
-                <input type="text" placeholder="Nome"/>
-                <button type="button" class='button'>
-                    Filtrar por nome
-                </button>
-            </form>
-            <h1> Registros de Categoria </h1>
-            <ul>
-                {categorias.map(categoria => (
-                <li key={categoria.id}>                                
-                    <b>Id da Categoria: </b>{categoria.codigoCategoria}<br></br>
-                    <b>Descrição: </b>{categoria.descricao}<br></br>
-                    <b>Data de Inclusão: </b>{categoria.dataInclusao}<br></br>
-                    <b>Ativo: </b>{categoria.ativo ? 'Sim' : 'Não'}<br></br>
-
-                    <button onClick={() => editAluno(categoria.id)} type="button">
-                        <FiEdit size="25" color="#17202a"/>
-                    </button>
-
+        return (
+            <div className="categoria-container">
+                <header>
+                    <img src={logoCadastro} alt="Cadastro"/>
+                    <Link className="button" to="/categoria/novo/0">Nova Categoria</Link>
                     <button type="button">
-                        <FiUserX size="25" color="#17202a"/>
+                        <FiXCircle size={40} color="#17202a"/>
                     </button>
-                </li>
-                ))}                
-            </ul>
-        </div>
-    )
-}
+                </header>
+
+                <form className="categoria-form">
+                    <input type="text" placeholder="Nome"/>
+                    <button type="button" class='button'>
+                        Filtrar por nome
+                    </button>
+                </form>
+                <h1> Registros de Categoria </h1>
+                <ul>
+                    {categorias.map(categoria => (
+                    <li key={categoria.id}>                                
+                        <b>Id da Categoria: </b>{categoria.codigoCategoria}<br></br>
+                        <b>Descrição: </b>{categoria.descricao}<br></br>
+                        <b>Data de Inclusão: </b>{categoria.dataInclusao}<br></br>
+                        <b>Ativo: </b>{categoria.ativo ? 'Sim' : 'Não'}<br></br>
+
+                        <button onClick={() => editCategoria(categoria.codigoCategoria)} type="button">
+                            <FiEdit size="25" color="#17202a"/>
+                        </button>
+
+                        <button type="button">
+                            <FiUserX size="25" color="#17202a"/>
+                        </button>
+                    </li>
+                    ))}                
+                </ul>
+            </div>
+        )
+    }
--- Front-End/formulario-react-v2/src/pages/NovaCategoria/index.js
+++ Front-End/formulario-react-v2/src/pages/NovaCategoria/index.js
@@ -8,6 +8,8 @@ export function NovaCategoria(){
 
     const [id, setId] = useState(null);
     const [descricao, setDescricao] = useState('');
+    const [dataInclusao, setDataInclusao] = useState(null);
+    const [ativo, setAtivo] = useState(null);
     const {categoriaId} = useParams();
     const navegar = useNavigate();
 
@@ -16,7 +18,7 @@ export function NovaCategoria(){
             return;
         else
             loadCategoria();
-    }, categoriaId);
+    }, categoriaId)
 
     async function loadCategoria(){
         try {
@@ -25,28 +27,30 @@ export function NovaCategoria(){
             setDescricao(response.data.descricao);
         }catch(error){
             alert('Erro ao recuperar a categoria ' + error);
-            navegar.push('/categoria');
+            navegar('/categoria');
         }
     }    
 
     async function saveOrUpdate(event){
         const data = {
             descricao,
+            dataInclusao,
             ativo
         }
 
         try{
             if(categoriaId === '0'){
-                await api.post('https://localhost:7290/api/supermercado/', data); 
+                await api.post('https://localhost:7290/api/supermercado/Categoria', data); 
+                alert("Categoria criada com sucesso !!");
             }
             else{
                 data.id = id;
-                await api.put(`https://localhost:7290/api/supermercado/${alunoId}`, data);
+                await api.put('https://localhost:7290/api/supermercado/Categoria', data);
             }            
         }catch(error){
             alert('Erro ao recuperar a categoria ' + error);
         }
-        Navigate.push('/categoria');
+        navegar('/categoria');
     }
 
     return(
@@ -54,24 +58,18 @@ export function NovaCategoria(){
             <div className="content">                
                 <section className="form">    
                     <FiUserPlus size={105} color="#17202a"></FiUserPlus>      
-                    <h1>{categoriaId === '0'? 'Incluir Nova Categoria' : 'Atualizar Categoria'}</h1>
+                    <h1>{categoriaId === '0'? 'Incluir Nova Categoria' : `Atualizar Categoria: ${categoriaId}`}</h1>
                     <Link className="back-link" to="/categoria">
                         <FiCornerDownLeft size={25} color="#17202a"/>
                         Retornar
                     </Link>          
                 </section>
                 
-                <form>
-                    <input placeholder="Descricão">
+                <form onSubmit={saveOrUpdate}>                                    
+                    <input placeholder="Descricão"
                         value={descricao}
-                        onchange={e=> setDescricao(e.target.value)}
-                    </input>
-                    {/* CORRIGIR */}
-                    <input placeholder="Data de Inclusão">
-                        value={null}
-                        onchange={e=> setDataInclusao(e.target.value)}
-                    </input>
-                    <input placeholder="Ativo"></input>
+                        onChange={e=> setDescricao(e.target.value)}
+                    />                                        
                     <button className="button" type="submit">{categoriaId === '0'? 'Incluir' : 'Atualizar'}</button>
                 </form>
             </div>
